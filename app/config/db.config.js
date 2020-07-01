bcfe65484310745aece302a9af5ab70e1f2f3631
Breno Nunes <breno.arantes@wexinc.com>
2020-06-30 23:02:51 -0300

    Change to postgress

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb",
-  dialect: "mysql",
+  HOST: 'localhost',
+  USER: 'kzmyciraobjdum',
+  PASSWORD: 'ff950f92cc48e0ddb686a8c62887e24317c0db313945299896432ab8909cb7a9',
+  DB: 'db3ioi6en02too',
+  dialect: 'postgres',
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 };
